477bb2ff7ab5a73291eccee6d840f92c2cc110b8
1ghostly <70901923+1ghostly@users.noreply.github.com>
2020-09-25 17:10:10 +0100

    Update SwordReachFinal.lua

--- Reach/SwordReachFinal.lua
+++ Reach/SwordReachFinal.lua
@@ -2,5 +2,6 @@ SwordHitBoxSize = 8
 SelectionBoxColor = Color3.fromRGB(255, 255, 255)
 SelectionBoxLineThickness = 1
 SelectionBoxTransparency = 0.5
+getgenv().LoopKill = false;
 
 loadstring(game:HttpGet(('https://raw.githubusercontent.com/1ghostly/ghostly-lua/master/ghostly-sword-reach.lua'),true))()
