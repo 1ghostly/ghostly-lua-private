6e797cd8de52e6f49a36f104921eb075504ce02f
1ghostly <70901923+1ghostly@users.noreply.github.com>
2020-09-27 21:57:37 +0100

    Update SwordReachFinal.lua

--- Reach/SwordReachFinal.lua
+++ Reach/SwordReachFinal.lua
@@ -1,7 +1,26 @@
-SwordHitBoxSize = 8
+--[[-Sword Settings-]]--
+
+--Change number values to your desired value
+
+SwordHitBoxSize = 99
 SelectionBoxColor = Color3.fromRGB(255, 255, 255)
-SelectionBoxLineThickness = 1
+SelectionBoxLineThickness = 0.1
 SelectionBoxTransparency = 0.5
-getgenv().LoopKill = false;
+
+--[[-Options-]]--
+
+--Change false to your desired option
+
+getgenv().BringPlayers = false
+getgenv().FOVChangerEnabled = false
+
+--[[-FOV Settings-]]--
+
+--Change number values to your desired value
+--70 is the default FOV
+
+FOV = 80
+
+--[[-Loader-]]--
 
 loadstring(game:HttpGet(('https://raw.githubusercontent.com/1ghostly/ghostly-lua/master/ghostly-sword-reach.lua'),true))()
